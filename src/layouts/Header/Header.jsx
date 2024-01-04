5785f57a9a2a38e588e33c867a06e9e3427916ca
Luis Garcia <luisarmandogarcia96@gmail.com>
2024-01-04 16:53:42 +0100

    Add logout functionality to Header component

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,9 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logOut } from "../../features/authSlice";
 import Logo from "../../assets/img/argentBankLogo.png";
 
 import styles from "./Header.module.scss";
 
 const Header = () => {
+	const navigate = useNavigate();
+	const { token } = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
+
+	//Handle Logout
+	const handleLogout = () => {
+		dispatch(logOut());
+		navigate("/login");
+	};
+
 	return (
 		<header className={styles.header}>
 			<Link to="/">
@@ -12,7 +24,9 @@ const Header = () => {
 
 			<div className={styles.header__signIn}>
 				<i className="fa-solid fa-circle-user"></i>
-				<Link to="/login">Sign In</Link>
+				{token && <Link to="/user">My Account</Link>}
+				{token && <span onClick={handleLogout}>Sign Out</span>}
+				{!token && <Link to="/login">Sign In</Link>}
 			</div>
 		</header>
 	);
