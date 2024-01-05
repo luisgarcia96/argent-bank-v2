24bfa85e89c054f5a2fd26fcea4af0c765a420e1
Luis Garcia <luisarmandogarcia96@gmail.com>
2024-01-05 13:17:04 +0100

    Create and Add userSlice to store.js

---
+++ src/features/userSlice.js
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getUser } from "../api/apiService";
+
+const initialState = {
+	user: null,
+};
+
+export const fetchUserInfo = createAsyncThunk(
+	"user/fetchUserInfo",
+	async (token, thunkAPI) => {
+		try {
+			const response = await getUser(token);
+			return response;
+		} catch (error) {
+			console.log("error", error);
+			return thunkAPI.rejectWithValue(error);
+		}
+	}
+);
+
+const userSlice = createSlice({
+	name: "user",
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchUserInfo.pending, (state) => {
+				state.user = null;
+			})
+			.addCase(fetchUserInfo.fulfilled, (state, action) => {
+				state.user = action.payload.body;
+			})
+			.addCase(fetchUserInfo.rejected, (state) => {
+				state.user = null;
+			});
+	},
+});
+
+export default userSlice.reducer;
